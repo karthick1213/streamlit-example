d256826dc4c7032e241ff474497c49bc46e9ed54
karthick1213 <85412087+karthick1213@users.noreply.github.com>
2023-10-25 12:30:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,192 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from scipy.special import softmax
+import matplotlib.pyplot as plt
+import numpy as np
 
-"""
-# Welcome to Streamlit!
+# Load the sentiment analysis model
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model2 = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+labels = ['Negative 😞', 'Neutral 😐', 'Positive 😀']
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.title("A STUDY ON SENTIMENTAL ANALYSIS OF MENTAL ILLNESS, CONNOTATIONS OF TEXTS")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Input area
+st.header("Input your tweets here (comma-separated):")
+user_input = st.text_area("Enter tweets:")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Analyze sentiment
+if st.button("Predict Sentiment"):
+    tweets = user_input.split(',')
 
+    # Lists to store sentiment scores
+    negative_scores = []
+    neutral_scores = []
+    positive_scores = []
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    for i, tweet in enumerate(tweets, start=1):
+        # Preprocess tweet
+        tweet = tweet.strip()  # Remove leading/trailing spaces
+        tweet_words = []
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        for word in tweet.split(' '):
+            if word.startswith('@') and len(word) > 1:
+                word = '@user'
+            elif word.startswith('http'):
+                word = "http"
+            tweet_words.append(word)
 
-    points_per_turn = total_points / num_turns
+        tweet_proc = " ".join(tweet_words)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        # Sentiment analysis
+        encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+        output = model2(**encoded_tweet)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        scores = output.logits[0].detach().numpy()
+        scores = softmax(scores)
+
+        negative_scores.append(scores[0])
+        neutral_scores.append(scores[1])
+        positive_scores.append(scores[2])
+
+    # Display sentiment scores and create a bar chart
+    st.subheader("Sentiment Analysis Results:")
+    for i, tweet in enumerate(tweets, start=1):
+        st.write(f"**Tweet {i}:** '{tweet}'")
+        st.write("Sentiment Scores:")
+        st.write(f"- Negative 😞: {negative_scores[i-1]:.5f}")
+        st.write(f"- Neutral 😐: {neutral_scores[i-1]:.5f}")
+        st.write(f"- Positive 😀: {positive_scores[i-1]:.5f}")
+
+    # Create a bar chart for sentiment scores
+    fig, ax = plt.subplots(figsize=(10, 6))
+    fig.patch.set_facecolor('yellow')
+    x = np.arange(len(tweets))
+    colors = ['red', 'gray', 'green']
+    bar_width = 0.2
+
+    ax.bar(x, negative_scores, width=bar_width, label='Negative 😞', color=colors[0])
+    ax.bar(x + bar_width, neutral_scores, width=bar_width, label='Neutral 😐', color=colors[1])
+    ax.bar(x + 2 * bar_width, positive_scores, width=bar_width, label='Positive 😀', color=colors[2])
+
+    ax.set_xlabel('Tweets')
+    ax.set_ylabel('Sentiment Scores')
+    ax.set_title('Sentiment Analysis Results')
+    ax.set_xticks(x + bar_width)
+    ax.set_xticklabels(tweets, rotation=46, ha="right")
+    ax.legend()
+
+    # Display the bar chart using st.pyplot()
+    st.pyplot(fig)
+
+# Add a clear button to reset the input and results
+if st.button("Clear"):
+    user_input = ""  # Clear user input
+    tweets = []  # Clear stored tweets
+    negative_scores = []  # Clear sentiment scores
+    neutral_scores = []
+    positive_scores = []
+
+# Display the code
+st.code("""
+import streamlit as st
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from scipy.special import softmax
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the sentiment analysis model
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+model2 = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+labels = ['Negative 😞', 'Neutral 😐', 'Positive 😀']
+
+st.title("A STUDY ON SENTIMENTAL ANALYSIS OF MENTAL ILLNESS, CONNOTATIONS OF TEXTS")
+
+# Input area
+st.header("Input your tweets here (comma-separated):")
+user_input = st.text_area("Enter tweets:")
+
+# Analyze sentiment
+if st.button("Predict Sentiment"):
+    tweets = user_input.split(',')
+
+    # Lists to store sentiment scores
+    negative_scores = []
+    neutral_scores = []
+    positive_scores = []
+
+    for i, tweet in enumerate(tweets, start=1):
+        # Preprocess tweet
+        tweet = tweet.strip()  # Remove leading/trailing spaces
+        tweet_words = []
+
+        for word in tweet.split(' '):
+            if word.startswith('@') and len(word) > 1:
+                word = '@user'
+            elif word.startswith('http'):
+                word = "http"
+            tweet_words.append(word)
+
+        tweet_proc = " ".join(tweet_words)
+
+        # Sentiment analysis
+        encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+        output = model2(**encoded_tweet)
+
+        scores = output.logits[0].detach().numpy()
+        scores = softmax(scores)
+
+        negative_scores.append(scores[0])
+        neutral_scores.append(scores[1])
+        positive_scores.append(scores[2])
+
+    # Display sentiment scores and create a bar chart
+    st.subheader("Sentiment Analysis Results:")
+    for i, tweet in enumerate(tweets, start=1):
+        st.write(f"**Tweet {i}:** '{tweet}'")
+        st.write("Sentiment Scores:")
+        st.write(f"- Negative 😞: {negative_scores[i-1]:.5f}")
+        st.write(f"- Neutral 😐: {neutral_scores[i-1]:.5f}")
+        st.write(f"- Positive 😀: {positive_scores[i-1]:.5f}")
+
+    # Create a bar chart for sentiment scores
+    fig, ax = plt.subplots(figsize=(10, 6))
+    fig.patch.set_facecolor('yellow')
+    x = np.arange(len(tweets))
+    colors = ['red', 'gray', 'green']
+    bar_width = 0.2
+
+    ax.bar(x, negative_scores, width=bar_width, label='Negative 😞', color=colors[0])
+    ax.bar(x + bar_width, neutral_scores, width=bar_width, label='Neutral 😐', color=colors[1])
+    ax.bar(x + 2 * bar_width, positive_scores, width=bar_width, label='Positive 😀', color=colors[2])
+
+    ax.set_xlabel('Tweets')
+    ax.set_ylabel('Sentiment Scores')
+    ax.set_title('Sentiment Analysis Results')
+    ax.set_xticks(x + bar_width)
+    ax.set_xticklabels(tweets, rotation=46, ha="right")
+    ax.legend()
+
+    # Display the bar chart using st.pyplot()
+    st.pyplot(fig)
+
+# Add a clear button to reset the input and results
+if st.button("Clear"):
+    user_input = ""  # Clear user input
+    tweets = []  # Clear stored tweets
+    negative_scores = []  # Clear sentiment scores
+    neutral_scores = []
+    positive_scores = []
+""")
+
+# Print the code
+st.button("Print Code")
+st.echo()
+st.write(" MINI PROJECT WORK DONE BY")
+st.write("KARTHICK-RA2232014010042")
+st.write("NIRANJAN-RA2232014010043")
+st.write("JIVITESH-RA2232014010048")
+st.write("HARISH-RA2232014010053")
